563286b32e25181cc91504351e944cdae7d3a8ba
깡단 <yyb19920702@gmail.com>
2026-07-14 10:54:21 +0900

    init

---
+++ src/main/java/com/example/coffeeordersystem/CoffeeOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeeordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeOrderSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coffeeordersystem/CoffeeOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeeordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
