a80c4dd0c1acf75b354fbe4cf2aab56085604e5a
깡단 <yyb19920702@gmail.com>
2026-07-19 16:23:35 +0900

    fix: 주석 설명 수정

--- src/main/java/com/example/coffeeordersystem/CoffeeOrderSystemApplication.java
+++ src/main/java/com/example/coffeeordersystem/CoffeeOrderSystemApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-// EnableScheduling: ProcessedEvent(랭킹 ledger) 보존기간 정리 배치(#126)에 사용한다.
+// EnableScheduling: ProcessedEvent(랭킹 ledger) 보존기간 정리 배치(ProcessedEventRetentionService)에 사용한다.
 @SpringBootApplication
 @EnableScheduling
 public class CoffeeOrderSystemApplication {
